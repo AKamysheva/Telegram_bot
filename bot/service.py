d0fe29715cccf39f997dc825e81383c577ac0f55
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2026-02-04 15:27:15 +0400

    added async openai, logger and changed model

--- bot/service.py
+++ bot/service.py
@@ -1,19 +1,22 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 from bot.config import settings
 from bot.prompt import get_prompt
+from bot.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 async def generate_sql(question: str):
-    print("Got message:", question)
-    client = OpenAI(
+    logger.info(f"Got message: {question}")
+    client = AsyncOpenAI(
         base_url="https://openrouter.ai/api/v1",
         api_key=settings.OPENROUTER_API_KEY,
     )
 
     prompt = f"{get_prompt()} Вопрос: {question}"
 
-    completion = client.chat.completions.create(
-        model="mistralai/devstral-2512:free",
+    completion = await client.chat.completions.create(
+        model="arcee-ai/trinity-large-preview:free",
         messages=[{"role": "user", "content": prompt}],
         extra_headers={
             "HTTP-Referer": "http://localhost",
@@ -22,6 +25,7 @@ async def generate_sql(question: str):
     )
     sql_query = completion.choices[0].message.content.strip()
     sql_query = sql_query.replace("```sql", "").replace("```", "").strip()
-    print(sql_query)
+
+    logger.info(f"SQL: {sql_query}")
 
     return sql_query
